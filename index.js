d995c5066ec5e21783302d6194559eaf3035bec5
Ian <ian.njoroge2@student.moringaschool.com>
2025-03-14 12:02:56 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  const drivers1 = ["Alice", "Benedict", "Ben", "John"];
+console.log(findMatching(drivers, "Ben"));
+
+
+
+function fuzzyMatch(drivers, searchString) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(searchString.toLowerCase()));
+}
+
+console.log= fuzzyMatch;
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
+console.log = matchName;
